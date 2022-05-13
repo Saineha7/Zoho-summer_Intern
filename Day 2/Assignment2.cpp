d46b66ef1d198cc465f92a3507d8f8cffa11904b
Saineha <84062876+Saineha7@users.noreply.github.com>
2022-05-13 12:12:35 +0530

    Update Assignment2.cpp

--- Day 2/Assignment2.cpp
+++ Day 2/Assignment2.cpp
@@ -1,38 +1,20 @@
 #include<iostream>
 #include<vector>
+#include<string>
 #include<stdlib.h>
 using namespace std;
 
 class Bank
 {
-	protected:
+	public:
 		string name, estDate, bankType;
 		float intRate;
 	public:
-		string getName();
-		string getEstablishedDate();
-		string BankType();
-		void getInfo();
-		friend Bank operator<(Bank &b1, Bank &b2);
+		virtual string getName()=0;
+		virtual string getEstablishedDate()=0;
+		virtual string BankType()=0;
+		virtual void getInfo()=0;
 };
-
-string Bank::getName()
-{
-	string str = "Enter bank name: ";
-	return str;
-}
-
-string Bank::getEstablishedDate()
-{
-	string str = "Enter Bank established date: ";
-	return str;
-}
-
-string Bank::BankType()
-{
-	string str = "Enter bank type: ";
-	return str;
-}
 
 class Loan
 {
@@ -268,6 +250,9 @@ string bname;
 
 class Broker: public HDFC, SBI, ICICI, IndianBank
 {
+    private: 
+        int x,y,z,a;
+        HDFC h;SBI s;ICICI i;IndianBank ib;
 	public:
 	friend Bank operator<(Bank &b1, Bank &b2);
 	
@@ -306,29 +291,109 @@ class Broker: public HDFC, SBI, ICICI, IndianBank
 		printICICI();
 		printIndianBank();
 	}
+
+    int compare(float x, float y)
+    {
+        if(x==999 || y==999)
+        return -1;
+        if(x < y )
+            return 1;
+        else 
+            return 2;
+    }
+
+    int compare(float x, float y, float z)
+    {
+        if(x==999 || y==999 || z==999)
+        return -1;
+        if(x < y && x < z)
+            return 1;
+        else if(y < x && y < z)
+            return 2;   
+        else
+            return 3;  
+    }
+
+    int compare(float x, float y, float z, float a)
+    {
+        if(x==999 || y==999 || z==999 || a==999)
+        return -1;
+        if(x < y && x < z && x < a)
+            return 1;
+        else if(y < x && y < z && y < a)
+            return 2;   
+        else if(z < x && z < y && z < a)
+            return 3; 
+        else
+            return 4;    
+    }
+
+    int check1(string bank1)
+    {
+        if(bank1=="HDFC" || bank1=="hdfc")
+            return h.intRate;
+        else if(bank1=="SBI" || bank1=="sbi")
+            return s.intRate;
+        else if(bank1=="ICICI" || bank1=="icici")
+            return i.intRate;
+        else if(bank1=="Indian Bank" || bank1=="indian bank")
+            return ib.intRate; 
+        else
+            return 999;  
+    }         
+
+    int check2(string bank2)
+    {
+        if(bank2=="HDFC" || bank2=="hdfc")
+            return h.intRate;
+        else if(bank2=="SBI" || bank2=="sbi")
+            return s.intRate;
+        else if(bank2=="ICICI" || bank2=="icici")
+            return i.intRate;
+        else if(bank2=="Indian Bank" || bank2=="indian bank")
+            return ib.intRate; 
+         else
+            return 999;
+ 
+    } 
+
+    int check3(string bank3)
+    {
+        if(bank3=="HDFC" || bank3=="hdfc")
+            return h.intRate;
+        else if(bank3=="SBI" || bank3=="sbi")
+            return s.intRate;
+        else if(bank3=="ICICI" || bank3=="icici")
+            return i.intRate;
+         else if(bank3=="Indian Bank" || bank3=="indian bank")
+            return ib.intRate;             
+        else 
+            return 999;
+  
+    }
+
+    int check4(string bank4)
+    {
+        if(bank4=="HDFC" || bank4=="hdfc")
+            return h.intRate;
+        else if(bank4=="SBI" || bank4=="sbi")
+            return s.intRate;
+        else if(bank4=="ICICI" || bank4=="icici")
+            return i.intRate;
+        else if(bank4=="Indian Bank" || bank4=="indian bank")
+            return ib.intRate;             
+        else
+            return 999;
+    }
 	
 };
-
-Bank operator<(Bank &b1, Bank &b2)	
-{
-	if(b1.intRate < b2.intRate)
-	{
-		bname = b1.name;
-		return b1;
-	}	
-	else
-	{
-		bname = b2.name;
-		return b2;	
-	}
-}
 
 int main()
 {
 	int ch;	char o;
 	do
 	{
-		system("clear");
+		system("CLS");
 		cout<<"\n 1. HDFC"<<endl;
 		cout<<"\n 2. SBI"<<endl;
 		cout<<"\n 3. ICIC"<<endl;
@@ -369,30 +434,72 @@ int main()
 					}
 					
 			case 5: { 
-					  int n;
-					  Bank t1, t2, t3; HDFC h; SBI s; ICICI i; IndianBank ib; 
+					  int n, o;Broker b;
+                      string bank1, bank2, bank3, bank4;
+                      cout<<"\n Available banks are: 1. HDFC 2. SBI 3. ICICI 4.Indian Bank"<<endl;
 					  cout<<"\n Enter no of banks to compare: ";
 					  cin>>n; 
 					  if(n==2)
 					  {
-					  	t1 = h < s;
-					  	cout<<"\n Between HDFC and SBI "<<bname<<" interest rate is cheaper"<<endl;
+                        cout<<"\n Enter Bank1 name: ";
+                        getline(cin >> ws, bank1);
+                        cout<<"\n Enter Bank2 name: ";
+                        getline(cin >> ws, bank2);   
+
+                        o = b.compare(b.check1(bank1), b.check2(bank2));
+                        if(o==1)
+                            cout<<bank1<<" interest rate is cheaper than"<<bank2<<endl;
+                        else if(o==2)
+                            cout<<bank2<<" interest rate is cheaper than "<<bank1<<endl;   
+                        else 
+                            cout<<"\n Incorrect bank name - Try again!"<<endl;
 					  }
 					  else if(n==3)
 					  {
-						t1 = h < s;
-						t2 = i < t1;
-						cout<<"\n Among HDFC, SBI and ICICI "<<bname<<" interest rate is cheaper"<<endl;
+                        cout<<"\n Enter Bank1 name: ";
+                        getline(cin >> ws, bank1);
+                        cout<<"\n Enter Bank2 name: ";
+                        getline(cin >> ws, bank2);
+                        cout<<"\n Enter Bank3 name: ";
+                        getline(cin >> ws, bank3);     
+
+                        o = b.compare(b.check1(bank1), b.check2(bank2), b.check3(bank3));
+                        if(o==1)
+						 cout<<bank1<<" interest rate is cheaper than"<<bank2<<" and "<<bank3<<endl;
+                        else if(o==2) 
+                            cout<<bank2<<" interest rate is cheaper than"<<bank1<<" and "<<bank3<<endl;
+                        else if(o==3)
+                            cout<<bank3<<" interest rate is cheaper than"<<bank1<<" and "<<bank2<<endl; 
+                        else 
+                            cout<<"\n Incorrect bank name - Try again!"<<endl;       
 					  }
+
 					  else
 					  {
-					  	t1 = h < s;
-					  	t2 = i < t1;
-					  	t3 = ib < t2;
-						cout<<"\n Among HDFC, SBI, ICICI and Indian Bank"<<bname<<" interest rate is cheaper"<<endl;   
+					  	cout<<"\n Enter Bank1 name: ";
+                        getline(cin >> ws, bank1);
+                        cout<<"\n Enter Bank2 name: ";
+                        getline(cin >> ws, bank2);
+                        cout<<"\n Enter Bank3 name: ";
+                        getline(cin >> ws, bank3);
+                        cout<<"\n Enter bank4 name: ";
+                        getline(cin >> ws, bank4);
+						             
+
+                        o = b.compare(b.check1(bank1), b.check2(bank2), b.check3(bank3), b.check4(bank4));
+                        if(o==1)
+						 cout<<bank1<<" interest rate is cheaper than"<<bank2<<" ,"<<bank3<<" and "<<bank4<<endl;
+                        else if(o==2) 
+                            cout<<bank2<<" interest rate is cheaper than"<<bank1<<" ,"<<bank3<<" and "<<bank4<<endl;
+                        else if(o==3)
+                            cout<<bank3<<" interest rate is cheaper than"<<bank1<<" ,"<<bank2<<" and "<<bank4<<endl;
+                        else if(o==4)
+                            cout<<bank4<<" interest rate is cheaper than"<<bank1<<" ,"<<bank2<<" and "<<bank3<<endl;
+                        else 
+                            cout<<"\n Incorrect bank name - Try again!"<<endl;            
 					  }
 					      break;
-					  }		
+					}		
 					  
 			case 6:{
 					   Broker b;
