655a2edaece8ccfc90e9c8815777a6dc9c6b1ff6
Saineha <84062876+Saineha7@users.noreply.github.com>
2022-05-16 15:59:03 +0530

    Update Assignment8.cpp

--- Day 8/Assignment8.cpp
+++ Day 8/Assignment8.cpp
@@ -8,8 +8,9 @@ struct numbers
     string mobile;
     string std;
     string landline;
-}num;
+}num[10];
 
+bool flag=0;int index1=0; int index2=0;
 class mobileConnection
 {
     public: 
@@ -34,21 +35,19 @@ class mobileConnection
                     mobileBillAmount = 0;
                     cout<<"\n Updated mobile bill amount: "<<mobileBillAmount<<endl;
                 }
+                flag=1;
             }
-            else
-                cout<<"\n Invalid connection type!"<<endl;
         }
 
-      void addConnection(string no)
+      void addConnection(int i, string no)
       {
           if(no.length()==10)
           {
-            num.mobile = mobileNo = no;
+            num[i].mobile = mobileNo = no;
             cout<<"\n Mobile no: "<<mobileNo<<endl;
             cout<<"\n New mobile connection added successfully!"<<endl;
+            flag=1;
           }
-          else
-            cout<<"\n Incorrect, enter a valid mobile number"<<endl;
       }
 };
 
@@ -83,23 +82,24 @@ class landlineConnection
                         cout<<"\n Updated landline bill amount: "<<landlineBillAmount<<endl;
                     }
                 }
+                flag=1;
             }
-            else 
-                cout<<"\n Invalid connection type!"<<endl;
+
         }
 
-         void addConnection(string no)
+         void addConnection(int i, string no)
         {
-          if(no.length()>=6)
+          if(no.size()>=6)
           {
-            num.std = stdCode = no.substr(0, (no.length()-6));      // leave out the last 6 digits and stor the rest of the value as std code
-            num.landline = landlineNo = (no.substr(no.length()-6)); //  get the last 6 digits and store as landline number
+            stdCode = no.substr(0, (no.length()-6));
+            num[i].std =  stdCode;// leave out the last 6 digits and stor the rest of the value as std code
+            landlineNo = (no.substr(no.length()-6)); //  get the last 6 digits and store as landline number
+            num[i].landline = landlineNo;
             cout<<"\n STD code: "<<stdCode<<endl;
             cout<<"\n Landline no: "<<landlineNo<<endl;
             cout<<"\n New landline connection added successfully!"<<endl;
+            flag=1;
           }
-          else
-            cout<<"\n Incorrect, enter a valid landline number"<<endl;
         }
 };
 
@@ -109,9 +109,9 @@ class billPayments
     public:
         T obj;
     public:
-        void storeConnections(string no)
+        void storeConnections(int i, string no)
         {
-            obj.addConnection(no);
+            obj.addConnection(i,no);
         }
         void updateBillAmount(string no)
         {
@@ -126,10 +126,11 @@ class billPayments
 int main()
 {
     string no,std; int n,op,ch; char o;
-    billPayments<mobileConnection> m;
-    billPayments<landlineConnection> l;
+    billPayments<mobileConnection> m[10];
+    billPayments<landlineConnection> l[10];
     do
     {
+        
         cout<<"\n 1. Add a new mobile connection"<<endl;
         cout<<"\n 2. Add a new landline connection"<<endl;
         cout<<"\n 3. Pay bill using the connection number"<<endl;
@@ -139,41 +140,89 @@ int main()
 
         switch(ch)
         {
-            case 1: cout<<"\n Enter number: ";
-                    cin>>no;
-                    m.storeConnections(no);
+            case 1: cout<<"\n Enter number of connections: ";
+                    cin>>n;
+                    flag=0;
+                    for(int i=0;i<n;index1++,i++)
+                    {
+                        cout<<"\n Enter number "<<index1+1<<" :";
+                        cin>>no;
+                        if(no.length()==10)
+                            m[index1].storeConnections(index1,no);
+                        else
+                            {
+                                cout<<"\n Incorrect, enter a valid mobile number"<<endl;
+                                break;
+                            }    
+                    }
                     break;
 
-            case 2: 
-                    cout<<"\n Enter number along with STD code: ";
-                    cin>>no;
-                    l.storeConnections(no);   
+            case 2: cout<<"\n Enter number of connections: ";
+                    cin>>n;flag=0;
+                    for(int i=0;i<n;index2++,i++)
+                    {
+                        cout<<"\n Enter number along with STD code: "<<index2+1<<" :";
+                        cin>>no;
+                        if(no.length()>=6)
+                            l[index2].storeConnections(index2,no);  
+                        else{ 
+                            cout<<"\n Incorrect, enter a valid landline number"<<endl;
+                            break;}
+                    }
                     break;
 
-            case 3: cout<<"\n Enter number for which to pay bill: ";
-                    cin>>no;
-                    if(num.mobile == no)
-                        m.zeroBillAmount(no);
-                    else if(num.landline == (no.substr(no.length()-6)))
-                        l.zeroBillAmount(no);
-                    else
-                        cout<<"\n Invalid connection type!"<<endl;      
+            case 3: 
+                    cout<<"\n Enter number for which to pay bill: ";
+                    cin>>no;flag=0;
+                    for(int i=0;i<index1;i++)
+                    {
+                        if(num[i].mobile == no)
+                            m[i].zeroBillAmount(no);
+                        else if(no.length()>=6 && (num[i].landline == (no.substr(no.length()-6))))
+                            l[i].zeroBillAmount(no);
+                        else
+                            continue;
+                    }
+                    for(int i=0;i<index2;i++)
+                    {
+                        if(num[i].mobile == no)
+                            m[i].zeroBillAmount(no);
+                        else if(no.length()>=6 && (num[i].landline == (no.substr(no.length()-6))))
+                            l[i].zeroBillAmount(no);
+                        else
+                            continue;
+                    }    
+                    if(!flag) cout<<"\n Incorrect, enter a valid number"<<endl;
                     break;
 
-            case 4:  cout<<"\n Enter number for which to update bill: ";
-                    cin>>no;
-                    if(num.mobile == no) 
-                        m.updateBillAmount(no);
-                    else if(num.landline == (no.substr(no.length()-6)))
-                        l.updateBillAmount(no);     
-                    else
-                        cout<<"\n Invalid connection type!"<<endl;    
-                    break;     
-
+            case 4:  
+                        cout<<"\n Enter number for which to update bill: ";
+                        cin>>no;flag=0;
+                        for(int i=0;i<index1;i++)
+                        {
+                            if(num[i].mobile == no) 
+                                m[i].updateBillAmount(no);
+                            else if(no.length()>=6 && (num[i].landline == (no.substr(no.length()-6))))
+                                l[i].updateBillAmount(no);     
+                            else    
+                                continue; 
+                        }
+                         for(int i=0;i<index2;i++)
+                        {
+                            if(num[i].mobile == no) 
+                                m[i].updateBillAmount(no);
+                            else if(no.length()>=6 && (num[i].landline == (no.substr(no.length()-6))))
+                                l[i].updateBillAmount(no);     
+                            else    
+                                continue; 
+                        }
+                        if(!flag) cout<<"\n Incorrect, enter a valid number"<<endl;
+                        break;
+                 
             default: cout<<"\n Invalid option, enter a valid one"<<endl;        
         }
         cout<<"\n Do you want to continue(y/n): ";
         cin>>o;
     }while(o=='y');   
-    return 0;  
+    return 0;
 }
