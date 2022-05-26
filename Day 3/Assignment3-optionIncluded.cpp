8adefc1c637215b94239fb22817e507d482fea90
Saineha <84062876+Saineha7@users.noreply.github.com>
2022-05-26 09:43:17 +0530

    Update Assignment3-optionIncluded.cpp

--- Day 3/Assignment3-optionIncluded.cpp
+++ Day 3/Assignment3-optionIncluded.cpp
@@ -10,6 +10,8 @@ struct coordinates{
     int y;
 };
 
+// Given - Write a function that will return an array with group members count and number of groups formed, so 
+// have created this struct.
 struct result
 {
     unique_ptr<int[]> arr;
@@ -20,7 +22,7 @@ class city
 {
     
     public:
-        int Nrows, Mcols, group=0, leaderGroup;
+        int Nrows, Mcols, group=0, leaderGroup, leaderGroupindex;
         unique_ptr<unique_ptr<int[]>[]>     buildings, visited;
         unique_ptr<int[]>                   families, tempVisit;
     public:
@@ -62,11 +64,11 @@ void city::formMatrix()
         cout<<"\n Enter values for row "<<i+1<<": "<<endl;
         for(int j=0; j<Mcols; j++)
         {
-            cin>>noOfPersons;
+            cin>>noOfPersons;               // get the no of persons per family
             families[j] = noOfPersons;
         }
-        buildings[i] = move(families);
-        visited[i] = move(tempVisit);
+        buildings[i] = move(families);      // move 1D array value into 2D
+        visited[i] = move(tempVisit);       // visited matrix initialization
     }
 }
 
@@ -78,27 +80,23 @@ void city::Grouping(int i, int j, int flag)
 	}
 	else
 	{
-        //int flag = 1;
 		group += buildings[i][j];
+        visited[i][j] = flag;
 		if(i+1 < Nrows && buildings[i+1][j] !=0) // explore downwards the neihbours of 1 entry
 		{
-			visited[i][j] = flag;
 			Grouping(i+1,j,flag); // recursively explore all the values of column and add to grpSize
 		}
 		// if adjacent element is 0 then we won't explore furthermore
 		if(i-1 >= 0 && buildings[i-1][j] !=0) // explore upwards of an element
 		{
-			visited[i][j] = flag;
 			Grouping(i-1,j,flag);
 		}
 		if(j+1 < Mcols && buildings[i][j+1] !=0) // explore horizontally (RHS)
 		{
-			visited[i][j] = flag;
 			Grouping(i,j+1,flag); 
 		}
 		if(j-1 >= 0 && buildings[i][j-1] !=0) // explore LHS of an element
 		{
-			visited[i][j] = flag;
 			Grouping(i,j-1,flag);
 		}
 		return;
@@ -107,25 +105,18 @@ void city::Grouping(int i, int j, int flag)
 
 void city::findTotalGroups()
 {
-    for(int i=0;i<Nrows;i++)
-    {
-        for(int j=0;j<Mcols;j++)
-        {
-            visited[i][j]=0;
-        }
-    }
     s.arr = make_unique<int[]>(10);int x;int flag=0;
     for(int i=0;i<Nrows;i++)
     {
         for(int j=0;j<Mcols;j++)
         {
-            if(buildings[i][j]!=0)
+            if(buildings[i][j]!=0)          // if not an empty building
             {
-                Grouping(i,j,s.count+1);
-                if(group!=0)
+                Grouping(i,j,s.count+1);    // then group families 
+                if(group!=0)                // if group formed 
                 {
-                    x = ++s.count;
-                    s.arr[x] = group; 
+                    x = ++s.count;          // increase the group count by 1
+                    s.arr[x] = group;       // push the members count in each group 
                 }
                 group = 0; 
             }
@@ -145,10 +136,12 @@ void city::findLeaderGroup()
 {
     for(int i=1;i<=s.count;i++)
     {
-        if(s.arr[i]>leaderGroup)
+        if(s.arr[i]>leaderGroup){    // check the array which has the members count in each group
             leaderGroup=s.arr[i];
+            leaderGroupindex = i;}
     }
     cout<<"\n Leader group: "<<leaderGroup<<endl;
+    cout<<"\n Leader group index: "<<leaderGroupindex<<endl;
 }
 
 void city::disp()
@@ -171,8 +164,6 @@ void city::printVisitMatrix()
     {
         for(int j=0;j<Mcols;j++)
         {
-            if(buildings[i][j]!=0 && visited[i][j]==0)
-                visited[i][j]=-1; // Assign -1 to all the solo buildings with no neighbours
             cout<<visited[i][j]<<" ";
         }
         cout<<endl;
@@ -182,53 +173,56 @@ void city::printVisitMatrix()
 void city::shortestDistMatrix()
 {
     vector<coordinates> coord;
-    for (int i = 0; i < Nrows; i++)
+    vector<vector<coordinates>> groupCoordinates(10);
+    for(int i=0;i<Nrows;i++)
     {
-        for (int j = 0; j < Mcols; j++)
+        for(int j=0;j<Mcols;j++)
         {
-           
-            if (visited[i][j] == 1)    
+            if (visited[i][j] == leaderGroupindex)    
             {
-                coordinates c;
+                coordinates c;                      // c has leader group coordinates
                 c.x = i;
                 c.y = j;
                 coord.push_back(c);
             }
+            
+            else if(visited[i][j]!=0 && visited[i][j]!=leaderGroupindex)
+            {
+                coordinates g;                      // g has other group coordinates
+                int temp=visited[i][j];
+                g.x = i;
+                g.y = j;
+                groupCoordinates[temp].push_back(g);
+            }
         }
+    
     }
 
-        int size = coord.size();
+    int size = coord.size();int z;int i;
 
     vector<int> dist; int min=0;
     cout<<"\n Path: ";
-    for(int z=2; z<=s.count; z++)
+
+   for(int i=1;i<=s.count;i++)      // s.count = no of groups 
     {
-        for (int i = 0; i < Nrows; i++)
+        if(i!=leaderGroupindex){
+        int distance = 999;
+        for(int j=0;j<groupCoordinates[i].size();j++)
         {
-            for (int j = 0; j < Mcols; j++)
+            
+            for (int k = 0; k < size; k++)  // k is used to access leader group coordinates
             {
-                if (visited[i][j] == z || visited[i][j] == -1) 
-                {
-
-                    int distance = 999;
-
-                    for (int k = 0; k < size; k++)
-                    {
-                        int d = (abs(i-coord[k].x) + abs(j-coord[k].y)-1);
-                        if (d < distance)
-                            distance = d;
-                    }
-                    dist.push_back(distance);
-                } 
+                int d = (abs((groupCoordinates[i][j].x)-coord[k].x) + abs((groupCoordinates[i][j].y)-coord[k].y)-1);
+                if (d < distance)
+                    distance = d;
             }
+                    
         }
-        for(int i=0;i<dist.size()-1;i++)
-        {
-            if(dist[i]<dist[i+1])
-            min = dist[i];
+        dist.push_back(distance);
         }
-        cout<<min<<" ";
-    }    
+    }
+    for(int i=0;i<dist.size();i++)
+    cout<<dist[i]<< " ";  
 }
 
 int main()
